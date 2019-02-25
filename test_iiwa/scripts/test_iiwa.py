6bc34ba97487bc34b091823346a33e457fb4e69c
chw <877869715@qq.com>
2019-02-25 15:29:20 +0800

    重构测试代码，新增launch文件

--- test_iiwa/scripts/test_iiwa.py
+++ test_iiwa/scripts/test_iiwa.py
@@ -8,6 +8,23 @@ import moveit_commander
 from moveit_msgs.msg import RobotState
 from sensor_msgs.msg import JointState
 
+def cartesian_move(group, waypoints, rate):
+    ready_state = RobotState()
+    joint_state = JointState()
+    joint_state.name = group.get_active_joints()
+    joint_state.position = group.get_current_joint_values()
+    ready_state.joint_state = joint_state
+
+    # compute a path consised of straight line segments from a series of waypoints
+    (plan, fraction) = group.compute_cartesian_path(waypoints, 0.0001, 0)
+    print(len(plan.joint_trajectory.points))
+    print('path fraction: {}'.format(fraction))
+
+    # arg ref_state_in is the start state of robot
+    new_plan = group.retime_trajectory(ready_state, plan, 0.005)
+    group.execute(new_plan)
+    group.stop()
+
 def main():
     moveit_commander.roscpp_initialize(sys.argv)
     rospy.init_node('move_group_commander', anonymous=True)
@@ -17,9 +34,9 @@ def main():
     group.set_max_velocity_scaling_factor(0.005)
 
     # home, ready are predefined pose, defined when create moveit package
-    group.set_named_target('home')
-    group.go()
-    group.stop()
+    # group.set_named_target('home')
+    # group.go()
+    # group.stop()
     
     group.set_named_target('ready')
     group.go()
@@ -35,20 +52,7 @@ def main():
     ready_target_pose = copy.deepcopy(ready_pose)
     ready_target_pose.position.x -= 0.2
 
-    ready_state = RobotState()
-    joint_state = JointState()
-    joint_state.name = group.get_active_joints()
-    joint_state.position = group.get_current_joint_values()
-    ready_state.joint_state = joint_state
-
-    # compute a path consised of straight line segments from a series of waypoints
-    (plan, fraction) = group.compute_cartesian_path([ready_target_pose, ready_pose], 0.005, 0)
-    print('path fraction: {}'.format(fraction))
-
-    # arg ref_state_in is the start state of robot
-    new_plan = group.retime_trajectory(ready_state, plan, 0.005)
-    group.execute(new_plan)
-    group.stop()
+    cartesian_move(group, [ready_target_pose, ready_pose], 0.005)
 
 if __name__ == '__main__':
     main()
